78b5b1814e5c2d530cd014a4fb4ee79e1d1b3ce1
Ramiad, Mohnish <Mohnish.Ramiad@ceridian.com>
2018-07-12 09:52:29 +0400

    review

--- myfirstclass/myfirstclass/Class1.cs
+++ myfirstclass/myfirstclass/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace myfirstclass
+﻿namespace myfirstclass
 {
     public class Class1
     {
